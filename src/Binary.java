3d2c14bf7a0864b7cf2295e23134bc3008243495
avponomarev <knopikanic@gmail.com>
2018-12-16 16:58:53 +0300

    Initial commit

---
+++ src/Binary.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Binary
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("Введите бинарное число для конвертации:");
+
+        String n=s.nextLine();
+
+        System.out.println(Integer.parseInt(n,2));
+    }
+}
